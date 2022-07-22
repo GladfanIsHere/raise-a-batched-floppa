f9ca10667dcd727e30fa091726af70e6a3f57c69
GladfanIsHere <96852415+GladfanIsHere@users.noreply.github.com>
2022-07-22 15:35:12 -0500

    [v0.1.2] Updated RaBF.bat
    Updated RaBF.bat code to make ythe source code be up to date

--- RaBF.bat
+++ RaBF.bat
@@ -3,13 +3,36 @@ title Raise a batched Floppa (RaBF)
 color 60
 mode con: cols=50 lines=20
 
+set /a SplashVar=%random% %%6
+if %SplashVar%==0 (
+    set SplashText=Terra will be soon...
+)
+if %SplashVar%==1 (
+    set SplashText=Created by GladfanIsHere!
+)
+if %SplashVar%==2 (
+    set SplashText=Sissio also takes care of Floppa
+)
+if %SplashVar%==3 (
+    set SplashText=Not made in Japan
+)
+if %SplashVar%==4 (
+    set SplashText=Doesn't includes Bingus!!
+)
+if %SplashVar%==5 (
+    set SplashText=Includes Floppa!!
+)
+
 ::Main menu related stuff
 :mainmenu
 color 60
 cls
 echo Raise a batched Floppa!
+echo.
+echo %SplashText%
+echo.
 echo         Version
-echo          0.1.1
+echo          0.1.2
 echo -----------------------
 echo 1) Play
 echo 2) Help
@@ -55,13 +78,12 @@ pause
 cls
 echo      Instructions
 echo -----------------------
-echo In this game you'll have to
-echo raise a Floppa, similar to
-echo it's roblox game, but made in
-echo batch, here you can't move so
-echo you'll have to select choices!
-echo Can you beat yourself raising
-echo Floppa?
+echo Raise your Floppa by taking care of him,
+echo and some random events will happen
+echo they may be good or bad!
+echo Beat yourself with the "score" feature
+echo Your best score will be saved so you
+echo can beat it with now problem!
 echo -----------------------
 echo 1) Return
 choice /c 1 /m "Return? "
@@ -76,7 +98,7 @@ echo        Credits
 echo -----------------------
 echo       Created by
 echo      GladfanIsHere
-echo    Load game bug fix
+echo      Contributors
 echo      Sissiogamer1
 echo -----------------------
 echo 1) Return
@@ -87,6 +109,7 @@ if %errorlevel% equ 1 (
 )
 
 :startoptions
+color 60
 cls
 echo      Start Options
 echo -----------------------
@@ -108,65 +131,153 @@ if %errorlevel% equ 3 (
 )
 pause
 
+::New "newgame" (Slot selector)
 :newgame
+set selectedSlot=0
+color 60
 cls
+echo      Game slots
+echo -----------------------
+echo 1) Slot 1
+echo 2) Slot 2
+echo 3) Slot 3
+echo 4) Return
+echo -----------------------
+choice /c 1234 /m "Which slot are you selecting? "
+if %errorlevel% equ 1 (
+    set selectedSlot=1
+    goto newstart
+)
+if %errorlevel% equ 2 (
+    set selectedSlot=2
+    goto newstart
+)
+if %errorlevel% equ 3 (
+    set selectedSlot=3
+    goto newstart
+)
+if %errorlevel% equ 4 (
+    goto startoptions
+)
+
+::New start (old :newgame)
+:newstart
+::-statsSlot
+::Floppa Stats
+set health=20
 set fhunger=100
 set fsanity=100
 set fage=1
 set factions=0
-(
-echo %fhunger%
-echo %fsanity%
-echo %fage%
-echo %factions%
-)> stats/foppastats.txt
+::Game stats
 set money=0
 set playerhasfood=0
-(
-echo %money%
-echo %playerhasfood%
-)> stats/gamestats.txt
-::player inventory stats
+set score=0
+set currentDM=None
+::-invStatsSlot
 set sanityPills=0
 set milk=0
 set birthdayCake=0
-set cellphone=0
-set foppaFren=0
+set mrbeastticket=0
+::End of "set" command
+::-"Exporting" data
+::Stats
 (
-echo %sanityPills%
-echo %milk%
-echo %birthdayCake%
-echo %cellphone%
-echo %foppaFren%
-)> invStats/invstats.txt
-pause
-goto loadgame
-::Load the game
+    echo %health%
+    echo %fhunger%
+    echo %fsanity%
+    echo %fage%
+    echo %factions%
+    echo %money%
+    echo %playerhasfood%
+    echo %score%
+    echo %currentDM%
+    echo %sanityPills%
+    echo %milk%
+    echo %birthdayCake%
+    echo %mrbeastticket%
+)> stats/slot%selectedSlot%/data.fdata
+::Best score
+set bestscore=0
+(
+    echo %bestscore%
+)> stats/bestSc.fdata
+::Death messages
+set dstarve=Your Floppa starved to death!
+set dhealth1=Your Floppa health was not good... 
+(
+    echo %dstarve%
+    echo %dhealth1%
+)> stats/deathMessages.fdata
+
+(
+    echo Yes
+)> stats/slot%selectedSlot%/inUse.fdata
+
+goto loadgamestart
+
+::New "loadgame" (Slot selector)
 :loadgame
+color 60
+set selectedSlot=0
 cls
-if exist stats/foppastats.txt (
+echo    Load a Game slot
+echo -----------------------
+echo 1) Slot 1
+echo 2) Slot 2
+echo 3) Slot 3
+echo 4) Return
+echo -----------------------
+choice /c 1234 /m "Which slot are you loading? "
+if %errorlevel% equ 1 (
+    set selectedSlot=1
+    goto loadgamestart
+)
+if %errorlevel% equ 2 (
+    set selectedSlot=2
+    goto loadgamestart
+)
+if %errorlevel% equ 3 (
+    set selectedSlot=3
+    goto loadgamestart
+)
+if %errorlevel% equ 4 (
+    goto startoptions
+)
+
+
+::Load the game (old :loadgame)
+:loadgamestart
+cls
+if exist stats/slot%selectedSlot%/inUse.fdata (
     (
+    set /p health=
     set /p fhunger=
     set /p fsanity=
     set /p fage=
     set /p factions=
-)< stats/foppastats.txt
-(
     set /p money=
     set /p playerhasfood=
-)< stats/gamestats.txt
-(
+    set /p score= 
+    set /p currentDM=
     set /p sanityPills=
     set /p milk=
     set /p birthdayCake=
-    set /p cellphone=
-    set /p foppaFren=
-)< invStats/invstats.txt
+    set /p mrbeastticket=
+)< stats/slot%selectedSlot%/data.fdata
+    (
+        set /p bestscore=
+    )< stats/bestSc.fdata
+    (
+        set /p dstarve=
+        set /p dhealth1=
+    )< stats/deathMessages.fdata
+
 goto welcome
 ) else (
     cls
     color c0
-    echo You didn't start any game yet.
+    echo This slot has no saved data.
     pause
     goto startoptions
 )
@@ -182,7 +293,10 @@ echo -----------------------
 echo Floppa hunger = %fhunger%
 echo Floppa sanity = %fsanity%
 echo Floppa age = %fage%
+echo Floppa health=%health%
 echo Your money = %money%$
+echo Score = %score%
+echo Best score = %bestscore%
 echo -----------------------
 echo These are the game stats
 
@@ -199,6 +313,9 @@ if %factions%==10 (
    echo Your Floppa age increased by 1
    echo and now it's %fage%!
 )
+if %health% GTR 20 (
+    set health=20
+)
 if %fhunger% GTR 100 (
    set fhunger=100
 )
@@ -206,45 +323,41 @@ if %fsanity% GTR 100 (
    set fsanity=100
 )
 if %fhunger% LSS 1 (
-   goto gameover
+    set currentDM=%dstarve%
+    goto gvsave1
+)
+if %health% LSS 1 (
+    set currentDM=%dhealth1%
+    goto gvsave1
 )
 if %fsanity% LSS 20 (
    set /a hunger=%hunger% - 10
+   set /a health=%health% - 5
    echo due to your Floppa's sanity
    echo being less than 20 your Floppa
-   echo lost 10 hunger!
+   echo lost 10 hunger and 5 health!
 )
 
 
 if %rnd1%==86 (
     goto travcake
 )
+if %rnd1%==67 (
+    goto travMrbtick
+)
+if %rnd1%==35 (
+    goto travBandits
+)
 if %fsanity% LEQ 0 (
     set fsanity=0
+)
+if %health% LEQ 0 (
+    set health=0
 )
 if %fhunger% LEQ 0 (
     set fhunger=0
 )
-if %foppaFren%==1 (
-    if %factions% equ 3 (
-        echo Your friend payed the rent
-        echo  Got 100$ from the rent
-        set /a money=%money% + 100
-    )
-    if %factions% equ 6 (
-       echo Your friend payed the rent
-        echo  Got 100$ from the rent
-        set /a money=%money% + 100
-    )
-    if %factions% equ 9 (
-        echo Your friend payed the rent
-        echo  Got 100$ from the rent
-        set /a money=%money% + 100
-    )
-) 
-if %foppaFren%==0 (
-    set foppaFren=0
-)
+set /a score=%fhunger% + %fsanity% * %fage% + %money% / 5
 cls
 color 0F
 echo  H=%fhunger% S=%fsanity% A=%fage% $=%money%
@@ -256,6 +369,8 @@ echo 4) Items
 echo 5) Save game
 echo 6) Go to main menu
 echo -----------------------
+echo Health=%health%
+echo Score=%score%
 echo.
 
 choice /c 123456 /m "What do you want to do?"
@@ -273,7 +388,7 @@ if %errorlevel% equ 4 (
     goto items
 )
 if %errorlevel% equ 5 (
-    goto save
+    goto save2
 )
 if %errorlevel% equ 6 (
     goto mainmenu
@@ -501,6 +616,7 @@ goto shop
 ::Items select menu
 
 :items
+set /a rndBeast=%random% %%1500
 set /a rnd2=%random% %%30
 cls
 echo        Items
@@ -509,12 +625,13 @@ echo 1) Return
 echo 2) Sanity pills
 echo 3) Milk
 echo 4) Birthday Cake
+echo 5) Mr.Beast event ticket
 echo -----------------------
 echo        Stats
 echo  H=%fhunger% S=%fsanity% A=%fage% $=%money%
 echo.
 
-choice /c 1234
+choice /c 12345
 if %errorlevel% equ 1 (
     goto game
 )
@@ -579,33 +696,67 @@ if %errorlevel% equ 4 (
         goto items
     )
 )
-::Cellphone
+::Mr beast ticket
 if %errorlevel% equ 5 (
-    if %cellphone%==1 (
+    if %mrbeastticket%==1 (
         cls
-        echo You called a roommate to stay in
-        echo your house, his rent will be 100$
-        echo every day that passes he'll pay you
-        echo             the rent!
+        echo You sent your Floppa to a Mr.Beast event,
+        echo and he won %rndBeast%$, got %rndBeast%$
+        set /a money=%money% + %rndBeast%
         pause
-        set foppaFren=1
-        goto save
+        goto gameover
     )
-    if %cellphone%==0 (
+    if %mrbeastticket%==0 (
         cls
-        echo You don't have a "Cellphone"!
+        echo You don't have a Ticket for the Mr.Beast event
         pause
         goto items
     )
 )
 
 ::Events
-
-:travPhone
+:travBandits
+set /a damageBandits=%random% %%10
 color b0
 cls
 echo Someone knocked at your door,
-echo Open the door?
+echo      Open the door?
+echo      1) Yes 2) No
+echo.
+choice /c 12 /m "Yes or no? "
+if %errorlevel% equ 1 (
+    cls
+    echo You open the door.
+    timeout /t 1 /nobreak > nul
+    cls
+    echo You open the door..
+    timeout /t 1 /nobreak > nul
+    cls
+    echo You open the door...
+    timeout /t 1 /nobreak > nul
+    cls
+    echo [!]Some bandits entered into your house and
+    echo       and hurted your Floppa!
+    echo Your Floppa lost %damageBandits% health...
+    set /a health=%health% - %damageBandits%
+    echo.
+    pause
+    goto save
+)
+if %errorlevel% equ 2 (
+    cls
+        echo You decided not opening the door, 
+        echo whoever who was there decided to
+        echo       go away in silent...
+        pause
+        goto save
+)
+
+:travMrbtick
+color b0
+cls
+echo Someone knocked at your door,
+echo      Open the door?
 echo      1) Yes 2) No
 echo.
 choice /c 12 /m "Yes or no? "
@@ -621,24 +772,25 @@ if %errorlevel% equ 1 (
     timeout /t 1 /nobreak > nul
     cls
     echo A traveller steped into your house and
-    echo  is selling you a Cellphone for only
-    echo     400$, Would you accept it?
+    echo  is selling you a ticket for the
+    echo    Mr.Beast event for only 700$
+    echo        Would you accept it?
+    echo.
     echo             1-Yes 2-No
     choice /c 12 /m "Yes or No? "
     if %errorlevel% equ 1 (
-        if %money% GEQ 400 (
+        if %money% GEQ 700 (
             cls
             echo You accepted the traveller's offer and
-            echo bought the Cellphone, you can use it to
-            echo call a roommate to stay in your house and
-            echo beg him to give you the rent for every day
-            echo that passes.
-            set /a money=%money% - 400
-            set cellphone=1
+            echo bought the ticket for the Mr.Beast event,
+            echo you can use it to make your Floppa go to
+            echo  a Mr.Beast event and win up to 1500$
+            set /a money=%money% - 700
+            set mrbeastticket=1
             pause
             goto save
         )
-        if %money% LSS 400 (
+        if %money% LSS 700 (
             cls
             echo You say to the traveller that you don't
             echo have enough money to accept his offer,
@@ -736,6 +888,7 @@ if %errorlevel% equ 2 (
         pause
         goto save
 )
+
 ::Game over!
 :gameover
 if %fsanity% LEQ 0 (
@@ -745,62 +898,164 @@ if %fhunger% LEQ 0 (
     set fhunger=0
 )
 cls
+if %score% GTR %bestscore% (
+    set bestscore=%score%
+    echo       New best!!
+) else (
+    echo.
+)
 color c0
 echo       GAME OVER!
-echo        Reason:
-echo Your Floppa starved to
-echo        death! 
+echo.
+echo %currentDM%
 echo -----------------------
 echo        Stats:
 echo   Floppa age = %fage%
 echo  Floppa sanity = %fsanity%
 echo  Floppa hunger = %fhunger%
+echo  Floppa health = %health%
+echo     Score = %score%
+echo  Best score = %bestscore%
 echo -----------------------
 echo 1) Go to main menu
-echo 2) Start a new game
+echo 2) Start a new game 
 echo.
 choice /c 12
 if %errorlevel% equ 1 (
-    goto mainmenu
+    set savebcdir=mainmenu
+    goto savebc
 )
 if %errorlevel% equ 2 (
-    goto newgame
+    set savebcdir=newgame
+    goto savebc
 )
 
-
 ::Save system related stuff
+:savebc
+set bestscore=%bestscore%
+(
+    echo %bestscore%
+)> stats/bestSc.fdata
+
+goto savebcdir
+
 :save
 cls
+set health=%health%
 set fhunger=%fhunger%
 set fsanity=%fsanity%
 set fage=%fage%
 set factions=%factions%
+set money=%money%
+set playerhasfood=%playerhasfood%
+set score=%score%
+set currentDM=%currentDM%
+set sanityPills=%sanityPills%
+set milk=%milk%
+set birthdayCake=%birthdayCake%
+set mrbeastticket=%mrbeastticket%
+
 (
+echo %health%
 echo %fhunger%
 echo %fsanity%
 echo %fage%
 echo %factions%
-)> stats/foppastats.txt
-set money=%money%
-set playerhasfood=%playerhasfood%
-(
 echo %money%
 echo %playerhasfood%
-)> stats/gamestats.txt
-::player inventory stats
-set sanityPills=%sanityPills%
-set milk=%milk%
-set birthdayCake=%birthdayCake%
-set cellphone=%cellphone%
-set foppaFren=%foppaFren%
-(
+echo %score%
+echo %currentDM%
 echo %sanityPills%
 echo %milk%
 echo %birthdayCake%
-echo %cellphone%
-echo %foppaFren%
-)> invStats/invstats.txt
+echo %mrbeastticket%
+)> stats/slot%selectedSlot%/data.fdata
+::Best score save
+set bestscore=%bestscore%
+(
+echo %bestscore%
+)> stats/bestSc.fdata
 
 goto game
+
+:save2
+cls
+set health=%health%
+set fhunger=%fhunger%
+set fsanity=%fsanity%
+set fage=%fage%
+set factions=%factions%
+set money=%money%
+set playerhasfood=%playerhasfood%
+set score=%score%
+set currentDM=%currentDM%
+set sanityPills=%sanityPills%
+set milk=%milk%
+set birthdayCake=%birthdayCake%
+set mrbeastticket=%mrbeastticket%
+
+(
+echo %health%
+echo %fhunger%
+echo %fsanity%
+echo %fage%
+echo %factions%
+echo %money%
+echo %playerhasfood%
+echo %score%
+echo %currentDM%
+echo %sanityPills%
+echo %milk%
+echo %birthdayCake%
+echo %mrbeastticket%
+)> stats/slot%selectedSlot%/data.fdata
+::Best score save
+set bestscore=%bestscore%
+(
+echo %bestscore%
+)> stats/bestSc.fdata
+
+echo The game was saved correctly!
+
+pause
+goto game
+
+:gvsave1
+cls
+set health=%health%
+set fhunger=%fhunger%
+set fsanity=%fsanity%
+set fage=%fage%
+set factions=%factions%
+set money=%money%
+set playerhasfood=%playerhasfood%
+set score=%score%
+set currentDM=%currentDM%
+set sanityPills=%sanityPills%
+set milk=%milk%
+set birthdayCake=%birthdayCake%
+set mrbeastticket=%mrbeastticket%
+
+(
+echo %health%
+echo %fhunger%
+echo %fsanity%
+echo %fage%
+echo %factions%
+echo %money%
+echo %playerhasfood%
+echo %score%
+echo %currentDM%
+echo %sanityPills%
+echo %milk%
+echo %birthdayCake%
+echo %mrbeastticket%
+)> stats/slot%selectedSlot%/data.fdata
+::Best score save
+set bestscore=%bestscore%
+(
+echo %bestscore%
+)> stats/bestSc.fdata
+goto gameover
 
 
